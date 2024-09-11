bcd2d7cd19a4b22179af802b114bb87592b47a2b
LocNguyen-807 <locb2111807@student.ctu.edu.vn>
2024-09-11 08:45:56 +0700

    Standardize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-
+const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
-    return res.json({
-    message: 'ok'
-    });
+        return res.json(JSend.success());
 });
-
-// app.get("/api/v1/contacts?favorite", function(req, res){
-
-// })
 
 contactsRouter.setup(app);
 
